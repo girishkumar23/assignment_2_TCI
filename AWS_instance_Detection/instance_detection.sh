7e4f12d0d81028c63ad04057b556533eb97932a0
girish <bvggk230391@gmail.com>
2021-06-22 22:53:03 +0530

    aws_ins

--- AWS_instance_Detection/instance_detection.sh
+++ AWS_instance_Detection/instance_detection.sh
@@ -1,8 +1,7 @@
 # install aws cli first and configure it with credentials and default region
 # the script will iterate over all regions of AWS
 
-aws ec2 describe-instances \ 
-aws ec2 describe-images \
+aws ec2 describe-instances \ describe-images 
     --filters Name=instance-type,Values=Name \ Name=image-type,Values=Name \
-    --query 'Reservations[*].Instances[*].Images[*].{OS:image-type,Machine Type:instance-type, Count: count(image-type)}'\
+    --query 'Reservations[*].Instances[*].{OS:image-type,Machine Type:instance-type, Count: count(image-type)}\
     --output table
